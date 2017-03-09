51b7ac64d4f21a8545033f276a3ba14465e8d7f9
victpf <ftpisbst@163.com>
2017-03-09 16:44:26 +0800

    add signup/signin/signout support

--- models/users.js
+++ models/users.js
@@ -4,5 +4,13 @@ module.exports = {
     // 注册一个用户
     create: function create(user) {
         return User.create(user).exec();
+    },
+    // 通过用户名获取用户信息
+    getUserByName: function getUserByName(name) {
+        return User
+            .findOne({ name: name })
+            .addCreatedAt()
+            .exec(); //返回来的user对象需要经过下面的几个函数的加工之后，在返回最后的对象
     }
+
 };
--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,13 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+    //  res.send(req.flash());  ---- 只返回空对象
+
+    // 清空 session 中用户信息
+    req.session.user = null;
+    req.flash('success', '登出成功啦！');
+    // 登出成功后跳转到主页
+    res.redirect('/posts');
 });
 
 module.exports = router;
